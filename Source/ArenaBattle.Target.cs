80b99489325782d9bf67d65353346dcd483da405
incar0615 <incar0615@gmail.com>
2017-03-07 14:05:35 +0900

    Add files via upload

--- Source/ArenaBattle.Target.cs
+++ Source/ArenaBattle.Target.cs
@@ -1,25 +1,25 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings. 
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class ArenaBattleTarget : TargetRules
 {
-	public ArenaBattleTarget(TargetInfo Target)
-	{
-		Type = TargetType.Game;
-	}
+    public ArenaBattleTarget(TargetInfo Target)
+    {
+        Type = TargetType.Game;
+    }
 
-	//
-	// TargetRules interface.
-	//
+    // 
+    // TargetRules interface. 
+    // 
 
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "ArenaBattle" } );
-	}
+    public override void SetupBinaries(
+        TargetInfo Target,
+        ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
+        ref List<string> OutExtraModuleNames
+        )
+    {
+        OutExtraModuleNames.AddRange(new string[] { "ArenaBattle", "WebService" });
+    }
 }
--- Source/ArenaBattle/ArenaBattle.cpp
+++ Source/ArenaBattle/ArenaBattle.cpp
@@ -1,5 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings. 
 
-#include "ArenaBattle.h"
+#include "ArenaBattle.h" 
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArenaBattle, "ArenaBattle" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ArenaBattle, "ArenaBattle");
